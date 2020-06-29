e9773ee1db8cd604f2396fa76c1b22acbab1d7e4
Marcelo Bressan <mbressan06@hotmail.com>
2020-06-29 12:30:43 -0300

    Add the create mongo collection script

--- db/create_mongo_collection.js
+++ db/create_mongo_collection.js
@@ -1,3 +1,3 @@
-// conn = new Mongo('mongodb://localhost:27017/adopets');  
+db.getSiblingDB("adopets").dropDatabase();
 db = connect("localhost:27017/adopets");
 db = db.createCollection( 'logger', {} )

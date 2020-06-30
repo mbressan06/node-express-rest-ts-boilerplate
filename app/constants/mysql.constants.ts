c2d6c9de80cc177dac87fb4f990448beff37f653
Marcelo Bressan <mbressan06@hotmail.com>
2020-06-30 15:50:43 -0300

    Add MySQL production credentials

--- app/constants/mysql.constants.ts
+++ app/constants/mysql.constants.ts
@@ -1,4 +1,4 @@
-export const HOST = 'localhost';
-export const USER = 'root';
-export const PASSWORD = '';
-export const DB = 'adopets';
+export const HOST = 'us-cdbr-east-02.cleardb.com';
+export const USER = 'b98b186ed954a9';
+export const PASSWORD = '81faa3cf';
+export const DB = 'heroku_de48e6bb7c3cf93';

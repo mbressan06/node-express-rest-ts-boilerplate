7c0bcb6ba3360b659fc4223bacd366deec2693e9
Marcelo Bressan <mbressan06@hotmail.com>
2020-06-30 16:49:02 -0300

    Added mysql connection config

--- app/config/mongo.config.ts
+++ app/config/mongo.config.ts
@@ -1,9 +1,7 @@
-import { MONGODB_HOST, MONGODB_USER, MONGODB_PASSWORD, MONGODB_COLLECTION } from '../constants/mongo.constants';
+import { MONGODB_HOST, MONGODB_COLLECTION } from '../constants/mongo.constants';
 
 const mongoConfig = {
   connectionString: MONGODB_HOST,
-// user: MONGODB_USER,
-// pass: MONGODB_PASSWORD,
   collection: MONGODB_COLLECTION
 };
 
--- app/constants/mongo.constants.ts
+++ app/constants/mongo.constants.ts
@@ -1,4 +1,2 @@
 export const MONGODB_HOST = process.env.MONGODB_URI || 'mongodb://localhost:27017/adopets';
-export const MONGODB_USER = '';
-export const MONGODB_PASSWORD = '';
 export const MONGODB_COLLECTION = process.env.MONGO_COL || 'logger';

207eb968fe41ad2dc0b6cb6d95887d0b4295e751
Marcelo Bressan <mbressan06@hotmail.com>
2020-06-30 16:30:02 -0300

    Added mongo prod config

--- app/constants/mongo.constants.ts
+++ app/constants/mongo.constants.ts
@@ -1,4 +1,4 @@
-export const MONGODB_HOST = 'mongodb://localhost:27017/adopets';
+export const MONGODB_HOST = process.env.MONGODB_URI || 'mongodb://localhost:27017/adopets';
 export const MONGODB_USER = '';
 export const MONGODB_PASSWORD = '';
-export const MONGODB_COLLECTION = 'logger';
+export const MONGODB_COLLECTION = process.env.MONGO_COL || 'logger';
